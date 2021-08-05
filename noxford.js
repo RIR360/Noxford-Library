95139abbd63ba02c4f5014d23d57147d269ab348
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2021-08-05 16:02:39 +0600

    routing

--- noxford.js
+++ noxford.js
@@ -9,16 +9,34 @@ const port = process.env.PORT || 3000;
 // app configurting
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public/css"));
-app.use("/img", express.static(__dirname + "public/img"));
 app.use(layouts);
 app.use(express.urlencoded({ extended: true }));
 
-// routing
+// ***** routing *****
+// homepage
 app.get("/", (req, res) => {
     res.render("home", {title: "Noxford Library"});
 });
-
+// collection
+app.get("/collection", (req, res) => {
+    res.render("collection", {title: "Noxford Library"});
+});
+// getcard
+app.get("/getcard", (req, res) => {
+    res.render("getcard", {title: "Get a card"});
+});
+// search
+app.get("/search", (req, res) => {
+    res.render("search", {title: "Search"});
+});
+// sign
+app.get("/sign", (req, res) => {
+    res.render("sign", {title: "Sign in"});
+});
+// donate
+app.get("/donate", (req, res) => {
+    res.render("donate", {title: "Donate"});
+});
 // 404 Error Page
 app.get("*", (req, res) => {
     res.render("notFound", {title: "404 Not Found"});
