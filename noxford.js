eea582738ed3d591a6a0f6cd8bfd147209ac9ba3
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2021-08-06 08:14:45 +0600

    log-reg

--- noxford.js
+++ noxford.js
@@ -12,14 +12,22 @@ app.use(express.static("public"));
 app.use(layouts);
 app.use(express.urlencoded({ extended: true }));
 
+// ***** Helper functions *****
+function error(res, message) {
+    if (!message) {
+        message = "Something wen't wrong";
+    }
+    res.render("error", {title: "Error", message: message});
+}
+
 // ***** routing *****
 // homepage
 app.get("/", (req, res) => {
-    res.render("home", {title: "Noxford Library"});
+    res.render("home", {title: "Home"});
 });
 // collection
 app.get("/collection", (req, res) => {
-    res.render("collection", {title: "Noxford Library"});
+    res.render("collection", {title: "Collection"});
 });
 // getcard
 app.get("/getcard", (req, res) => {
@@ -30,8 +38,46 @@ app.get("/search", (req, res) => {
     res.render("search", {title: "Search"});
 });
 // sign
-app.get("/sign", (req, res) => {
-    res.render("sign", {title: "Sign in"});
+app.get("/login", (req, res) => {
+    res.render("login", {title: "Login"});
+});
+app.post("/login", (req, res) => {
+
+    let email = req.body.email;
+    let pwd = req.body.password;
+
+    if (!email || !pwd) {
+        error(res);
+    }
+    else {
+        // log the user in
+        res.redirect("/");
+    }
+});
+// register
+app.get("/register", (req, res) => {
+    res.render("register", {title: "Register"});
+});
+app.post("/register", (req, res) => {
+
+    let fname = req.body.fname;
+    let email = req.body.email;
+    let pwd = req.body.password;
+    let c_pwd = req.body.confirm;
+
+    if (!fname || !email || !pwd) {
+        error(res);
+    }
+    else if (fname.length > 45) {
+        error(res, "Full name too long");
+    }
+    else if (pwd != c_pwd) {
+        error(res, "Password Did not match");
+    }
+    else {
+        // register the user
+        res.redirect("/");
+    }
 });
 // donate
 app.get("/donate", (req, res) => {
