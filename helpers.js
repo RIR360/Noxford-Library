b1957bb227ce2caf6ae56d47fc0c3769a5ba82f1
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2021-08-12 09:01:36 +0600

    log-reg func

---
+++ helpers.js
@@ -0,0 +1,18 @@
+// show a message page
+function log(res, message) {
+    if (!message) {
+        message = "Hello World!";
+    }
+    res.render("log", {title: "Log", message: message});
+}
+// show error page
+function error(res, message) {
+    if (!message) {
+        message = "Something wen't wrong";
+    }
+    res.render("error", {title: "Error", message: message});
+}
+ 
+module.exports = {
+    error, log
+};
--- noxford.js
+++ noxford.js
@@ -1,7 +1,11 @@
 // modules
-//const fsystem = require("fs");
+const fsystem = require("fs");
 const express = require("express");
 const layouts = require("express-ejs-layouts");
+const bcrypt = require("bcrypt");
+const Helper = require("./helpers");
+// constants
+const DATABASE = "database.json";
 
 // app
 const app = express();
@@ -12,14 +16,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(layouts);
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-// ***** Helper functions *****
-function error(res, message) {
-    if (!message) {
-        message = "Something wen't wrong";
-    }
-    res.render("error", {title: "Error", message: message});
-}
 
 // ***** routing *****
 // homepage
@@ -42,42 +38,112 @@ app.get("/search", (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login", {title: "Login"});
 });
-app.post("/login", (req, res) => {
-
-    let email = req.body.email;
-    let pwd = req.body.password;
-
+app.post("/login", async (req, res) => {
+    // get user credentials
+    const email = req.body.email;
+    const pwd = req.body.password;
+    // basic validation
     if (!email || !pwd) {
-        error(res);
+        Helper.error(res);
     }
     else {
         // log the user in
-        res.redirect("/");
+        // load up database
+        const database = JSON.parse(
+            fsystem.readFileSync(DATABASE)
+        );
+        var logger;
+        // check if user already exists
+        for (user of database)
+        {
+            if (user.email == email) {
+                logger = user;
+                break;
+            }
+        }
+        if (!logger) {
+            Helper.error(res, "User doesn't exists");
+        }
+        else {
+            // user found
+            if (await bcrypt.compare(pwd, logger.hash))
+            {
+                Helper.log(res, "Wow! you logged in");
+            }
+            else {
+                res.redirect("/login");
+            }
+        }
     }
 });
 // register
 app.get("/register", (req, res) => {
     res.render("register", {title: "Register"});
 });
-app.post("/register", (req, res) => {
-
-    let fname = req.body.fname;
-    let email = req.body.email;
-    let pwd = req.body.password;
-    let c_pwd = req.body.confirm;
-
+app.post("/register", async (req, res) => {
+    // get user inputs
+    const fname = req.body.fname;
+    const email = req.body.email;
+    const pwd = req.body.password;
+    const c_pwd = req.body.confirm;
+    // read the database
+    const database = JSON.parse(
+        fsystem.readFileSync(DATABASE)
+    );
+    // check if user already exists
+    for (user of database)
+    {
+        if (user.email == email)
+        {
+            Helper.error(res, "User already exists");
+            process.exit();
+        }
+    }
+    // validation check
     if (!fname || !email || !pwd) {
-        error(res);
+        Helper.error(res);
     }
     else if (fname.length > 45) {
-        error(res, "Full name too long");
+        Helper.error(res, "Full name too long");
     }
     else if (pwd != c_pwd) {
-        error(res, "Password Did not match");
+        Helper.error(res, "Password Did not match");
+    }
+    else if (pwd.length < 8 || pwd.length > 40)
+    {
+        Helper.error(res, "Password too small or big");
     }
     else {
         // register the user
-        res.redirect("/");
+        // encrypting password
+        try {
+            const pass = bcrypt.hash(
+                req.body.password,
+                10
+            );
+            // getting user data
+            const userData = {
+                fullname: req.body.fname,
+                email: req.body.email,
+                hash: await pass
+            };
+            // push  userdata in the database
+            database.push(userData);
+            // write database
+            fsystem.writeFileSync(
+                "database.json", 
+                JSON.stringify(database, null, 2), 
+                err => {
+                    if (err) {
+                        Helper.error(err);
+                    }
+                }
+            );
+            // go back to homepage
+            res.redirect("/");   
+        } catch (err) {
+            Helper.error(res, err);
+        }
     }
 });
 // donate
