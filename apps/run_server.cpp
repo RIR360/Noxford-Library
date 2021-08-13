93edc1cd2d45124ddedbbb66e31f737e0fe36b43
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2021-08-13 17:50:37 +0600

    server app

---
+++ apps/run_server.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<fstream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    string local_port = "3000";
+    string content = "[InternetShortcut]\nURL=http://localhost:";
+    content += local_port;
+
+    string file = "temp-link.url";
+    ofstream temp;
+    temp.open(file.c_str(), ios::out);
+    temp << content;
+
+    if(!temp)
+    {
+        cout << "Can't create link file.";
+        system("pause");
+        return 1;
+    }
+    else {
+        temp.close();
+        string del_file = "del /f " + file;
+
+        system(file.c_str());
+        system(del_file.c_str());
+        system("nodemon app");
+    }
+    return 0;
+}
